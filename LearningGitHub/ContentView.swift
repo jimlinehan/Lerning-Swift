8ac97fcbe34ebf1abbad7cd5afe2fa7316b715f3
Jim Linehan <jalinehan@comcast.net>
2023-02-21 16:11:05 -0500

    Initial Commit

---
+++ LearningGitHub/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LearningGitHub
+//
+//  Created by Jim Linehan on 2/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hi Jim Linehan, you made it!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Jim Linehan on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
