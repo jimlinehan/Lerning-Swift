2aa493168adab5e7fbc8db95fe494f98ee0e4dab
Jim Linehan <jalinehan@comcast.net>
2023-02-21 16:17:10 -0500

    I made changes to demonstrate how to commit change to GitHub

--- LearningGitHub/ContentView.swift
+++ LearningGitHub/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hi Jim Linehan, you made it!")
+            Text("Hi Jim Linehan, this is you first updated to GitHub")
         }
         .padding()
     }
